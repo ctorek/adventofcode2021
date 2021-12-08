314222d4608983a0fc72c68ef6dc5649acd5486c
ctorek <ctorek@protonmail.ch>
2021-12-07 22:40:11 -0500

    giving up to go play brawlhalla

--- day3.hs
+++ day3.hs
@@ -1,13 +1,20 @@
 import Data.List (genericLength, transpose)
 import Data.Char (intToDigit)
 
--- find most common out of a string of 0 or 1
-mostCommon :: String -> Char
-mostCommon l
-    | ones l > zeros l = '1'
-    | otherwise        = '0'
+-- find most common out of a string of 0 or 1 and a default if equal
+mostCommon :: String -> Char -> Char
+mostCommon l d
+    | ones l == zeros l = d
+    | ones l > zeros l  = '1'
+    | otherwise         = '0'
     where zeros x = count x '0'
           ones x = count x '1'
+
+-- gets opposite of most common
+leastCommon :: String -> Char -> Char
+leastCommon l d =
+    if (mostCommon l '1' == '1')
+      then '0' else '1'
 
 -- count appearances of element in list
 count :: (Eq a) => [a] -> a -> Int
@@ -15,7 +22,7 @@ count l val = length $ filter (== val) l
 
 -- find most common bit for each column in inputs
 gammaRate :: [String] -> String
-gammaRate l = map (mostCommon) l
+gammaRate l = map (\x -> mostCommon x '0') l
 
 -- opposite of gamma rate
 epsilonRate :: String -> String
@@ -26,12 +33,23 @@ epsilonRate l = map replace l
 toDecimal :: String -> Int
 toDecimal l = sum $ zipWith (\b ind -> (read b) * (2 ^ ind)) (reverse $ map (:[]) l) [0,1..]
 
+-- find most common bit in each position or default 1 if equal
+oxygenRate :: [String] -> String
+oxygenRate l = mostCommon (map head l) '1' : oxygenRate (map tail l)
+
+-- find least common bit in each position or default 0 if equal
+co2Rate :: [String] -> String
+co2Rate l = leastCommon (map head l) '0' : co2Rate (map tail l)
+
 main :: IO()
 main = do
     input <- getContents
+    let i = transpose $ lines input
     putStrLn "Part 1:"
-    let gamma = toDecimal $ gammaRate $ transpose $ lines input
-    let epsilon = toDecimal $ epsilonRate $ gammaRate $ transpose $ lines input
+    let gamma = toDecimal $ gammaRate i
+    let epsilon = toDecimal $ epsilonRate $ gammaRate i
     print (gamma * epsilon)
 
     putStrLn "Part 2:"
+    let oxygen = toDecimal $ oxygenRate i
+    print oxygen
